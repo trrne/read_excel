058056db607c70f5bb4ffab630bd19d97cffb85b
cet <cets021@gmail.com>
2023-11-01 23:05:47 +0900

    TODO: テキストボックスに入力できるようにして、入力された値分組み合わせを表示するように

--- main.py
+++ main.py
@@ -1,41 +1,99 @@
-import random
+from random import randint
 import openpyxl
-
-source_path = "ideas.xlsx"
-file = openpyxl.load_workbook(source_path, read_only=True)
-sheet = file["Sheet1"]
-row = sheet.max_row
-
-nouns = []
-verbs = []
-choice_nouns = []
-choice_verbs = []
+import typing
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
 
 
-def read_nouns():
-    for i in range(3, row, 1):
-        nouns.append(sheet.cell(i, 1).value)
-    return [n for n in nouns if n != '' or n != None]
+def ice(n: list) -> int:
+    return len(n)-1
 
 
-def read_verbs():
-    for i in range(3, row, 1):
-        verbs.append(sheet.cell(i, 2).value)
-    return [v for v in verbs if v != None]
-    # return [v for v in verbs if v != '' or v != None]
-    # vv = []
-    # for j in range(len(verbs)):
-    # if (len(j) >= 1):
-    # vv.append(verbs[j])
-    # return vv
+class IdeasListDict(typing.TypedDict):
+    ns: list
+    vs: list
+
+
+class Excel:
+    def __init__(self, source_path: str) -> None:
+        self.source_path: str = source_path
+        self.file: openpyxl.Workbook
+        self.sheet: openpyxl.openpyxl.worksheet.worksheet
+        self.row: int
+        self.column: int
+        self.loop_count: int
+        if self.source_path != None:
+            self.file = openpyxl.load_workbook(self.source_path)
+            self.sheet = self.file["Sheet1"]
+            self.row = self.sheet.max_row
+            self.column = self.sheet.max_column
+
+    def get_source_path(self) -> str:
+        return self.source_path
+
+    def get_row(self) -> int:
+        return self.row
+
+    def get_row_column(self) -> tuple[int, int]:
+        return (self.row, self.column)
+
+    def get_cell_value(self, row: int, column: int):
+        return self.sheet.cell(row, column).value
+
+    def set_loop_count(self, loop_count) -> None:
+        self.loop_count = loop_count
+
+    def read_data(self) -> IdeasListDict:
+        (n, v) = ([], [])
+        for i in range(3, self.get_row(), 1):
+            n.append(self.get_cell_value(i, 1))
+            v.append(self.get_cell_value(i, 2))
+        return IdeasListDict(
+            ns=[nouns for nouns in n if nouns != None],
+            vs=[verb for verb in v if verb != None]
+        )
+
+    # @staticmethod
+    def generate_pair(self):
+        rdata = self.read_data()
+        output_label['text'] = self.output(rdata)
+
+    def output(self, idea: IdeasListDict):
+        read_data = idea
+        (nouns, verbs) = (read_data['ns'], read_data['vs'])
+        data: IdeasListDict
+        for _ in range(LOOP_COUNT):
+            data = IdeasListDict(
+                ns=nouns[randint(0, ice(nouns))],
+                vs=verbs[randint(0, ice(verbs))]
+            )
+        return data
 
 
 LOOP_COUNT = 20
 if __name__ == "__main__":
-    n = read_nouns()
-    v = read_verbs()
-    for _ in range(LOOP_COUNT):
-        print(read_nouns()[random.randint(0, len(read_nouns())-1)],
-              "+", read_verbs()[random.randint(0, len(read_verbs())-1)])
-        # nn = n[random.randint(0, )]
-        # print()
+    root = tk.Tk()
+    root.title('アイデア出し')
+    root.geometry('800x600')
+
+    xlsx = filedialog.askopenfilename(
+        title='*.xlsxを開く',
+        filetypes=[('XLSX Worksheet', '*.xlsx')],
+        initialdir='./'
+    )
+    excel = Excel(xlsx)
+
+    name_label = tk.Label(root, text='選択しているファイル: ' + excel.source_path)
+    name_label.pack()
+
+    loop_input = tk.Entry(root)
+    loop_input.insert(tk.END, 20)
+    loop_input.pack()
+
+    output_label = tk.Label()
+    output_label.pack()
+    run_btn = tk.Button(root, text='生成', command=excel.generate_pair)
+    run_btn.pack()
+
+    root.mainloop()
