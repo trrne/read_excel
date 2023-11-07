605469528ae7c37d1c828a3b32e9648333c670ab
cet <cets021@gmail.com>
2023-11-08 08:56:54 +0900

    a

--- main.py
+++ main.py
@@ -18,12 +18,8 @@ from typing import (
 
 import tkinter as tk
 from tkinter import (
-    # messagebox,
+    messagebox,
     filedialog
-)
-
-from functools import (
-    singledispatch
 )
 
 
@@ -45,62 +41,55 @@ class Excel:
         self.source_path: str = source_path
         self.file: Workbook
         self.sheet: worksheet
+        self.sheets: list
         self.row: int
         self.column: int
-        self.count: int
+        self.loop: int
         if self.source_path != None:
             self.file = pyxl.load_workbook(self.source_path)
-            self.sheet = self.file["Sheet1"]
-            self.row = self.sheet.max_row
-            self.column = self.sheet.max_column
+            self.sheets = self.file.sheetnames
+            if (name := self.sheets[0]) != None:
+                self.sheet = self.file[name]
+                self.row = self.sheet.max_row
+                self.column = self.sheet.max_column
 
-    def get_source_path(self) -> str:
-        return self.source_path
-
-    def get_row(self) -> int:
-        return self.row
-
-    def get_cell_value(self, row: int, column: int) -> Any:
+    def cell_value(self, row: int, column: int) -> Any:
         return self.sheet.cell(row, column).value
 
-    def set_loop_count(self, loop_count) -> None:
-        self.count = loop_count
-
-    def get_loop_count(self) -> int:
-        return self.count
-
-    def read_data(self) -> IdeasListDict:
+    def read(self) -> IdeasListDict:
         (ns, vs) = ([], [])
-        for i in range(3, self.get_row(), 1):
-            ns.append(self.get_cell_value(i, 1))
-            vs.append(self.get_cell_value(i, 2))
+        for i in range(3, self.row, 1):
+            ns.append(self.cell_value(i, 1))
+            vs.append(self.cell_value(i, 2))
         return IdeasListDict(
             nouns=[n for n in ns if n != None], verbs=[v for v in vs if v != None])
 
     # @staticmethod
     def generate_pair(self) -> None:
-        rdata: IdeasListDict = self.read_data()
+        rdata: IdeasListDict = self.read()
         (nouns, verbs) = (rdata['nouns'], rdata['verbs'])
         donestr = []
-        for _ in range(self.get_loop_count()):
-            donestr.append(f'{nouns[choice(nouns)]} + {verbs[choice(verbs)]}')
-        output_label['text'] = str.join('\n', donestr)
+        try:
+            for _ in range(self.loop):
+                donestr.append(
+                    f'{nouns[choice(nouns)]} + {verbs[choice(verbs)]}')
+            output_label['text'] = str.join('\n', donestr)
+        except:
+            messagebox.showerror(
+                title='loop count is empty.', message='please enter it.')
 
 
 class MyApp(tk.Frame):
     def __init__(self, title: str, resolution: str) -> None:
-        self.__root = tk.Tk()
+        self.root = tk.Tk()
         self.title = title
         self.resolution = resolution
-        self.__root.title(self.title)
-        self.__root.geometry(self.resolution)
-        super().__init__(self.__root)
-
-    def root(self) -> tk.Tk:
-        return self.__root
+        self.root.title(self.title)
+        self.root.geometry(self.resolution)
+        super().__init__(self.root)
 
     def loop(self) -> None:
-        self.__root.mainloop()
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
@@ -113,18 +102,18 @@ if __name__ == "__main__":
     )
     excel = Excel(xlsx)
 
-    name_label = tk.Label(app.root(), text='選択しているファイル: ' + excel.source_path)
+    name_label = tk.Label(app.root, text='選択しているファイル: ' + excel.source_path)
     name_label.pack()
 
-    loop_input = tk.Entry(app.root(), width=20)
-    # loop_input.insert(tk.END, 20)
-    loop_input.insert('end', 20)
+    loop_input = tk.Entry(app.root, width=20)
+    loop_input.insert(tk.END, 20)  # 入力できない
     loop_input.pack()
-    excel.set_loop_count(int(loop_input.get()))
+
+    excel.loop = int(loop_input.get())
 
     output_label = tk.Label()
     output_label.pack()
-    run_btn = tk.Button(app.root(), text='生成', command=excel.generate_pair)
+    run_btn = tk.Button(app.root, text='生成', command=excel.generate_pair)
     run_btn.pack()
 
     app.loop()
